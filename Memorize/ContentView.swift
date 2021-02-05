38907d5efb8a1f24ed2391c206e5f570de437a96
ergenekonyigit <ergenekon.yigit@trendyol.com>
2021-02-06 01:06:50 +0300

    Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
             .padding()
+            .foregroundColor(.orange)
+            .font(.largeTitle)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill()
+            }
+        }
     }
 }
